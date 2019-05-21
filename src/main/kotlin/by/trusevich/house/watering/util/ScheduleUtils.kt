d8d099b38724bea66ec914fddce55ea194a64585
Valentin Trusevich <v.trusevich@itransition.com>
2019-05-21 16:15:56 +0300

    Fixed npe on delete

--- src/main/kotlin/by/trusevich/house/watering/util/ScheduleUtils.kt
+++ src/main/kotlin/by/trusevich/house/watering/util/ScheduleUtils.kt
@@ -1,7 +1,6 @@
 package by.trusevich.house.watering.util
 
 import java.util.concurrent.ScheduledFuture
-import javax.persistence.EntityNotFoundException
 
 private val SCHEDULERS_HOLDER = mutableMapOf<Long, ScheduledFuture<*>>()
 
@@ -9,7 +8,7 @@ fun removeScheduler(wateringId: Long) = SCHEDULERS_HOLDER[wateringId]?.let {
     it.cancel(false)
 
     SCHEDULERS_HOLDER.remove(wateringId)
-} ?: throw EntityNotFoundException()
+}
 
 fun saveScheduler(wateringId: Long, scheduledTask: ScheduledFuture<*>) =
     SCHEDULERS_HOLDER.put(wateringId, scheduledTask)
